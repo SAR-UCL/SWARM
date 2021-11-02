0c6a604476a59a5eb3c016cebdb4d533281917c6
Sachin Alexander Reddy <63003744+SAR-UCL@users.noreply.github.com>
2021-11-02 17:33:46 +0000

    02-Nov-21

---
+++ Download/open-ftp.py
@@ -0,0 +1,92 @@
+#https://github.com/pignalberi/TITIPy/blob/master/Downloading_Swarm_data.py
+
+from ftplib import FTP
+import os
+import zipfile
+import patoolib
+
+SATE = ['A','C']
+YEAR = [2017,2018,2019,2020]
+MONTH = [3,4,10,11,12]
+DAYS = 30
+
+for SAT in SATE:
+    for YR in YEAR:
+        for MON in MONTH:
+            for DOM in range(DAYS):
+                DOM = DOM + 1
+                
+                #path = '/Users/sr2/OneDrive - University College London/PhD/Research/Missions/SWARM/in-flight data/LP'  #Density and potential
+                #path = '/Users/sr2/OneDrive - University College London/PhD/Research/Missions/SWARM/in-flight data/EFI' #Ion temp
+                #path = '/Users/sr2/OneDrive - University College London/PhD/Research/Missions/SWARM/in-flight data/IPD' #Bubbles and IPIR
+                path = '/Users/sr2/OneDrive - University College London/PhD/Research/Missions/SWARM/in-flight data/IBI' #Bubbles and IPIR
+
+                path_initial = os.path.join(path,str(YR).zfill(4)+str(MON).zfill(2)+str(DOM).zfill(2)+str(SAT))
+                path_downloaded_data_LP=os.path.join(path_initial)
+                os.makedirs(path_downloaded_data_LP)
+
+                try:
+                    for sat in SAT:
+                        ftp = FTP('swarm-diss.eo.esa.int')   # connect to host, default port
+                        ftp.login()               # user anonymous, passwd anonymous
+                        
+                        
+                        #ftp.cwd('/Level1b/Latest_baselines/EFIxLPI/Sat_'+str(sat)) #Langmuir Probe (plasma den & space pot)
+                        #ftp.cwd('/Level2daily/Latest_baselines/EFI/TIE/Sat_'+str(sat)) #Electric field ins (ion temp)
+                        #ftp.cwd('/Level2daily/Latest_baselines/IPD/IRR/Sat_'+str(sat)) #Bubbles and iregularities
+                        ftp.cwd('/Level2daily/Latest_baselines/IBI/TMS/Sat_'+str(sat)) #Bubbles and iregularities
+                        
+                        
+                        listing=[]
+                        ftp.retrlines("LIST", listing.append)
+                        
+                        filenames=[]
+                        for index in listing:
+                            words=index.split(None, 8)
+                            if(words[-1].lstrip()[-4:]=='.ZIP'):
+                                filenames.append(words[-1].lstrip())
+                        
+                        #Checks if folder exists
+                        flag=False
+                        for filename in filenames:
+                            if(filename[19:23]==str(YR).zfill(4) and filename[23:25]==str(MON).zfill(2) and filename[25:27]==str(DOM).zfill(2)):
+                                file_founded=filename
+                                flag=True
+                                break
+                    
+                        os.chdir(path_downloaded_data_LP)
+
+                        print('Downloading file for Swarm '+str(sat)+' for '+str(YR).zfill(4)+'/'+str(MON).zfill(2)+'/'+str(DOM).zfill(2)+' ...')
+                        
+                        with open(file_founded, 'wb' ) as file :
+                                ftp.retrbinary('RETR %s' % file_founded, file.write)
+                                file.close()                
+
+                        if(not flag):
+                            print('File not found for Swarm '+str(sat)+' for '+str(YR).zfill(4)+'/'+str(MON).zfill(2)+'/'+str(DOM).zfill(2))
+                            pass
+                            #raise Exception('File not found for Swarm '+str(sat)+' for '+str(YR).zfill(4)+'/'+str(MON).zfill(2)+'/'+str(DOM).zfill(2))
+                            
+                        #os.chdir(main_folder) 
+
+                    ftp.close()
+                except RuntimeError:
+                    raise Exception('Problems in connecting to or downloading from Swarm FTP')
+
+                #Unzip and delete zip
+                try:
+                    swarm_files=os.listdir(path_downloaded_data_LP)
+                    swarm_files.sort()
+                    for file in swarm_files:
+                        patoolib.extract_archive(file, outdir=path_downloaded_data_LP,verbosity=-1)
+                        os.remove(file)
+
+                    files=os.listdir()
+                    for file in files:
+                        if(file[-4:]=='.cdf'):
+                            pass
+                        else:
+                            os.remove(file)
+                    print('Extraction for Swarm '+str(sat)+' for '+str(YR).zfill(4)+'/'+str(MON).zfill(2)+'/'+str(DOM).zfill(2)+' complete')
+                except RuntimeError:
+                    raise Exception('Problems extracting the SWARM file')
--- Extraction/extract_IPB.py
+++ Extraction/extract_IPB.py
@@ -10,8 +10,8 @@ import geopandas
 import time
 import zipfile
 
-IBI_dir = Path(r'/Users/sr2/OneDrive - University College London/PhD/Research/Missions/SWARM/Data/oct-21/IBI/3-year-C')
-csv_path = r'/Users/sr2/OneDrive - University College London/PhD/Research/Missions/SWARM/Non-Flight Data/Analysis/Oct-21/data'
+IBI_dir = Path(r'/Users/sr2/OneDrive - University College London/PhD/Research/Missions/SWARM/in-flight data/IBI')
+csv_path = r'/Users/sr2/OneDrive - University College London/PhD/Research/Missions/SWARM/Non-Flight Data/Analysis/Nov-21/data'
 
 
 def openIBI(dire):
@@ -25,6 +25,7 @@ def openIBI(dire):
         utc = cdf.varget("Timestamp") #select variables of interest
         lat = cdf.varget("Latitude")
         lon = cdf.varget("Longitude")
+        #loc = cdf.varget("Ionosphere_region_flag")
 
         #science
         bub_ind = cdf.varget("Bubble_Index")
@@ -33,27 +34,21 @@ def openIBI(dire):
         #flags
         bub_flag = cdf.varget("Flags_Bubble")
         mag_flag = cdf.varget("Flags_F")
-        #alt = cdf.varget("Radius")
-        #mlt = cdf.varget("MLT")
-        # This has value 0 (midnight) in the anti-sunward direction, 12 (noon) 
-        # in the sunward direction and 6 (dawn) and 
-        # 18 (dusk) perpendicular to the sunward/anti-sunward line.
-        #Tn = cdf.varget("Tn_msis")
-        #Ti = cdf.varget("Ti_meas_drift")
-        #TiM = cdf.varget("Ti_model_drift")
-
-        #flag 
-        #Ti_flag = cdf.varget("Flag_ti_meas")
 
         #place in dataframe
-        cdf_df = pd.DataFrame({'datetime':utc,'lat':lat, 'long':lon, 'b_ind':bub_ind, 'b_prob':bub_prob,
+        #cdf_df = pd.DataFrame({'datetime':utc,'lat':lat, 'long':lon, 'loc':loc, 'b_ind':bub_ind})
+        cdf_df = pd.DataFrame({'datetime':utc,'lat':lat, 'long':lon,'b_ind':bub_ind, 'b_prob':bub_prob,
             'bub_flag':bub_flag, 'mag_flag':mag_flag})
         cdf_array.append(cdf_df)
         ibi_data = pd.concat(cdf_array)
+
+
+        ibi_data = ibi_data.loc[ibi_data['b_ind'] == 1]
+        #ibi_data = ibi_data[ibi_data['long'].between(-75,45)]
         
         #Filters
-        ibi_data = ibi_data.loc[ibi_data['b_ind'] == 1]
-        ibi_data = ibi_data.loc[ibi_data['bub_flag'] == 2]
+        #ibi_data = ibi_data.loc[ibi_data['b_ind'] == 1]
+        #ibi_data = ibi_data.loc[ibi_data['bub_flag'] == 2]
         ibi_data = ibi_data.loc[ibi_data['b_prob'] > 0]
 
         
@@ -64,24 +59,24 @@ def openIBI(dire):
     return ibi_data
     #return efi_data #concat enables multiple .cdf files to be to one df
 
-
 #IBI_data = openIBI(IBI_dir)
 #print(IBI_data)
 
+
 #Loading and exporting
-#path = r'/Users/sr2/OneDrive - University College London/PhD/Research/Missions/SWARM/Instrument Data/Analysis/Sept-21/data/'
+path = r'/Users/sr2/OneDrive - University College London/PhD/Research/Missions/SWARM/Instrument Data/Analysis/Sept-21/data/'
 file_name = r'/IBI-data.csv'
 load_csv = csv_path + file_name
 load_csv = pd.read_csv(load_csv)
+#load_csv = load_csv.loc[load_csv['bub_flag'] == 1] #bubble flag 1 = confirmed bubble
 print(load_csv)
-
 
 
 #figs, axs = plt.subplots(ncols=1, nrows=1, figsize=(9.5,6.5), sharex=False, sharey=True) #3.5 for single, #5.5 for double
 #axs = axs.flatten()
-'''
-IBI_data.plot(kind="scatter", x="long", y="lat", alpha=0.4,
+
+load_csv.plot(kind="scatter", x="long", y="lat", alpha=0.4,
 c="b_prob", cmap=plt.get_cmap("jet"), colorbar=True)
 
 plt.legend()
-plt.show()'''
+plt.show()
