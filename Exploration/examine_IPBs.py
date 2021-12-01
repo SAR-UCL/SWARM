849bbfe76ae731f031d81e5f8d5a5a31b03179cc
Sachin Alexander Reddy <63003744+SAR-UCL@users.noreply.github.com>
2021-12-01 16:17:40 +0000

    1-Dec-21(2)

--- Exploration/examine_IPBs.py
+++ Exploration/examine_IPBs.py
@@ -23,12 +23,6 @@ class WrangleData():
         file_name = 'joined-data-'+ today +'.h5'
         return cls(hdf_path+file_name, select_date)
 
-    def classify_EPB(self, ne_std, b_ind, ti_std, pot_std):
-        #if ne_std > 0.095 or b_ind == 1:
-        if ne_std > 0.01 and ti_std > 0.01 and pot_std > 0.01:
-            return 1
-        else:
-            return 0
 
     def transform_EPB(self, sat, s_time, e_time):
         #Filter the data
@@ -37,104 +31,111 @@ class WrangleData():
         self.df = self.df[~self.df['mlt'].between(6,18)] #Nightime only
         self.df = self.df[self.df['s_id'] == sat]
         self.df = self.df[self.df['utc'].between(s_time, e_time)]
-        self.df = self.df[self.df['lat'].between(-30,30)] #EPB region 
+        self.df = self.df[self.df['lat'].between(-20,20)] #EPB region 
 
-        #Call classifier function
-        self.new_classifier()
-     
-    def new_classifier(self):
+        def new_classifier(df):
 
+                def classify_EPB(ne_std, b_ind, ti_std, pot_std):
+                        if ne_std > 0.01 and ti_std > 0.01 and pot_std > 0.01:
+                                return 1
+                        else:
+                                return 0
+
+
+                #New EPB classification
+                df['epb'] = df.apply(lambda x: classify_EPB(x.Ne_std, 
+                        x.b_ind, x.Ti_std, x.pot_std), axis=1)
+                #df = df[df['epb'] == 1]
+
+                #Calculate EPB start, middle and end
+                epb_cat = 'epb'
+                df['change'] = df[epb_cat] - df[epb_cat].shift(1)
+                df = df.replace({'change':{-1:2}})
+                df['temp'] = np.where((df[epb_cat] == 1) 
+                        & (df['change'] == 0), 1.5, 0)
+
+                #Reassign s, m & e to one column
+                #https://stackoverflow.com/questions/39109045/
+                #numpy-where-with-multiple-conditions
+                conditions = [df['change'] == 1, df['temp'] == 1.5, 
+                        df['change'] ==2]
+                output = [1,1.5,2]
+                df['cat'] = np.select(conditions, output, default = '0')
+
+                # if epb_only == True:
+                #     df = df[df['cat'].between('1','2')]
+                # else:
+                #     pass
+
+                df = df.reset_index().drop(columns=['change',
+                        'temp','index'], axis=1)
+
+
+                return df
         
-        #New EPB classification
-        self.df['epb'] = self.df.apply(lambda x: self.classify_EPB(x.Ne_std, 
-                x.b_ind, x.Ti_std, x.pot_std), axis=1)
-        #self.df = self.df[self.df['epb'] == 1]
-        
-        
-        #Calculate EPB start, middle and end
-        epb_cat = 'epb'
-        self.df['change'] = self.df[epb_cat] - self.df[epb_cat].shift(1)
-        self.df = self.df.replace({'change':{-1:2}})
-        self.df['temp'] = np.where((self.df[epb_cat] == 1) 
-                & (self.df['change'] == 0), 1.5, 0)
+        df = new_classifier(self.df)
+        #return df
 
-        #Reassign s, m & e to one column
-        #https://stackoverflow.com/questions/39109045/
-        #numpy-where-with-multiple-conditions
-        conditions = [self.df['change'] == 1, self.df['temp'] == 1.5, 
-                 self.df['change'] ==2]
-        output = [1,1.5,2]
-        self.df['cat'] = np.select(conditions, output, default = '0')
-
-        # if epb_only == True:
-        #     self.df = self.df[self.df['cat'].between('1','2')]
-        # else:
-        #     pass
-
-        self.df = self.df.reset_index().drop(columns=['change',
-                'temp','index'], axis=1)
-        
-
+ 
         #Determine range
-        def pre_post_EPB(df):
+        def pre_post_EPB(df):#
+                #cols = df.columns
+                cols = df.loc[:, df.columns != 'b_ind']
+                cols = df.loc[:, df.columns != 'b_prob']
+                cols = df.loc[:, df.columns != 'epb']
+                #print(cols)
+                #cols = cols.columns
+                cols = ['cat']
+                df_pre = df.loc['1':,cols].head(25)
 
+                print('df_pre\n', df_pre)
 
-            #cols = df.columns
-            cols = df.loc[:, df.columns != 'b_ind']
-            cols = df.loc[:, df.columns != 'b_prob']
-            cols = df.loc[:, df.columns != 'epb']
-            #print(cols)
-            #cols = cols.columns
-            cols = ['cat']
-            df_pre = df.loc['1':,cols].head(25)
+                df_post = df.loc[:'2',cols].tail(25)
+                
+                print('df_post\n', df_post)
 
-            print('df_pre\n', df_pre)
+                df = df[df['cat'].between('1','2')]
 
-            df_post = df.loc[:'2',cols].tail(25)
-            
-            print('df_post\n', df_post)
+                print(df)
 
-            df = df[df['cat'].between('1','2')]
+                df = pd.concat([df_pre, df, df_post], axis =0)
 
-            print(df)
+                df = df.sort_values(by=['utc'], ascending=True)
 
-            df = pd.concat([df_pre, df, df_post], axis =0)
+                print(df)
 
-            df = df.sort_values(by=['utc'], ascending=True)
+                #load_hdf = load_hdf.sort_values(by=['utc'], ascending = True)
 
-            print(df)
+                return df
 
-            #load_hdf = load_hdf.sort_values(by=['utc'], ascending = True)
-
-            return df
-
-        #self.df = pre_post_EPB(self.df)
+        #self.df = pre_post_EPB(df)
 
         self.df = self.df.drop_duplicates().dropna()
         self.df = self.df.reset_index().drop(columns=['index'], axis=1)
 
-        print(self.df)
-        return self.df
+        df = self.df
+
+        return df
     
 
 select_date = None
-select_date = '2016-04-07'
+select_date = '2016-04-01'
 w = WrangleData.frompath(hdf_path, select_date)
 
-sat = 'A'
-start_time = '09:05:32'
-end_time = '09:20:32'
+sat = 'C'
+start_time, end_time = '20:35:32', '20:47:32'
 epb_only = False
-#start_time = '00:00:00'
-#end_time = '23:59:59'
+#start_time, end_time = '00:00:00','23:59:59'
 cleaned_df = w.transform_EPB(sat, start_time, end_time)
-#print(cleaned_df)
+
+#print('Outside Class\n', cleaned_df)
 
 
 class PlotEPB():
 
     def __init__(self, df):
         self.df = df
+        print(df)
 
     def plotPanels(self):
 
@@ -186,7 +187,7 @@ class PlotEPB():
 
         
 
-        axs[0].set_title(f'{title}: from {date_s} at {utc_s}' 
+        axs[0].set_title(f'{title}: from {date_s} at {utc_s} ' 
                 f'to {date_e} at {utc_e} \n size: ~{epb_len} km', fontsize = 11)
         axs[0].set_ylabel('EPB Prob')
         axs[0].set_ylim(0, 1)
@@ -230,10 +231,10 @@ class PlotEPB():
         axs[7].set_ylabel('IPB Prob \n proposed')
         #axs[7].invert_xaxis()
 
-        n = len(self.df) // 8
+        #n = len(self.df) // 8
         #n = 50  # Keeps every 7th label
-        [l.set_visible(False) for (i,l) in 
-                enumerate(axs[7].xaxis.get_ticklabels()) if i % n != 0]
+        #[l.set_visible(False) for (i,l) in 
+        #        enumerate(axs[7].xaxis.get_ticklabels()) if i % n != 0]
         #axs[4].tick_params(axis='x',labelrotation=90)
         #ax[0].set_xticks[]
         #axs[0].set_xticks([], minor=False)
@@ -262,170 +263,6 @@ class PlotEPB():
         plt.show()
 
 
-p = PlotEPB(cleaned_df)
-panels = p.plotPanels()
+#p = PlotEPB(cleaned_df)
+#panels = p.plotPanels()
 #counts = p.plotEPBCount()
-
-#select_date = '2016-04-01'
-#w = wrangler.frompath(hdf_path, select_date)
-
-#p = plot()
-#plot_panels = p.plotPanels()
-#prepplots(w)
-
-# df = w.filter()
-# df['epb'] = df.apply(lambda x: w.classifyEPB(x.Ne_std, x.b_ind), axis=1)
-# print(df)
-
-#print('classy \n',filt)
-
-
-
-def tempCats(x, y):
-    if x > 0.095:
-        return 1
-    else:
-        return 0
-
-#joined_cdf['temp_prob'] = joined_cdf.apply(lambda x: tempCats(x.Ne_std, x.pot_std), axis=1)
-
-def newEPB(x, y):
-    import numpy as np
-    if x or y == np.abs(1):
-        return 1
-    else:
-        return 0
-
-#joined_cdf['n_prob'] = joined_cdf.apply(lambda x: newEPB(x.b_ind, x.temp_prob), axis=1)
-
-
-def removeSSA(df):
-    df = df[df['long'].between(10,180)]
-    return df
-#load_hdf = removeSSA(load_hdf)
-
-def dayNight(df):
-    df = df[~df['mlt'].between(6,18)]
-    return df
-#load_hdf = dayNight(load_hdf)
-
-#load_hdf = load_hdf[load_hdf['b_ind'] == 0]
-#load_hdf = load_hdf[load_hdf['b_prob'] >= 0.85]
-#load_hdf = load_hdf[load_hdf['b_prob'].between(0.8, 0.9)]
-#print(load_hdf)
-'''
-
-#load_hdf = load_hdf[load_hdf['utc'].between('00:36', '00:53')] #2016-04-03. This is in the supervisor presi on 17-11-21
-#load_hdf = load_hdf[load_hdf['utc'].between('00:40', '00:50')] #2016-04-03. This is in the supervisor presi on 17-11-21
-#load_hdf = load_hdf[load_hdf['utc'].between('1z:20', '19:34')] #2016-04-03b. This is in the supervisor presi on 17-11-21
-#load_hdf = load_hdf[load_hdf['lat'].between(-8, 28)] #2016-04-03b. This is in the supervisor presi on 17-11-21
-#load_hdf = load_hdf[load_hdf['utc'].between("20:18", "20:26")] #2016-04-04. 
-#load_hdf = load_hdf[load_hdf['utc'].between('21:30', '21:45:30')] #2017-04-07
-#load_hdf = load_hdf[load_hdf['utc'].between('21:13', '21:25')] #2016-04-05
-
-#Swarm C
-#load_hdf = load_hdf[load_hdf['utc'].between('00:06', '00:11:40')] #2019-03-16 slim
-#load_hdf = load_hdf[load_hdf['utc'].between('00:04', '00:12:20')] #2019-03-16
-#load_hdf = load_hdf[load_hdf['utc'].between('00:02', '00:14')] #2019-03-016 (wider)
-
-#Not an EPB
-#load_hdf = load_hdf[load_hdf['utc'].between('02:54', '03:10')] #2016-04-07
-
-#Swarm A
-#load_hdf = load_hdf[load_hdf['utc'].between('00:01:30', '00:12')] #2019-03-16
-
-#load_hdf = load_hdf[load_hdf['mlt'].between(0,6)]
-load_hdf = load_hdf[load_hdf['lat'].between(-30,30)]
-#load_hdf = load_hdf[load_hdf['Ne_c'] <= -0.4]
-
-#Not an EPB
-load_hdf = load_hdf[load_hdf['utc'].between('09:05:20', '09:20:50')] #2016-04-07
-
-
-#load_hdf = load_hdf[::120]
-load_hdf = load_hdf.sort_values(by=['utc'], ascending = True)
-load_hdf = load_hdf[load_hdf['s_id'] == "A"]
-print(load_hdf)
-#print(load_hdf.dtypes)
-
-
-figs, axs = plt.subplots(ncols=1, nrows=8, figsize=(10,7), dpi=90, sharex=True) #3.5 for single, #5.5 for double
-axs = axs.flatten()
-
-x = 'lat'
-#sns.set_palette("Reds")
-palette_ne, palette_ti, palette_pot, hue = 'Set1', 'Set2', 'tab10', 's_id'
-sns.lineplot(ax = axs[0], data = load_hdf, x = x, y ='b_prob', palette = 'bone_r', hue = hue, legend =False)
-sns.lineplot(ax = axs[1], data = load_hdf, x = x, y ='Ne', palette = palette_ne, hue = hue, legend = False)
-sns.lineplot(ax = axs[2], data = load_hdf, x = x, y ='Ne_std', palette = palette_ne, hue = hue, legend = False)
-sns.lineplot(ax = axs[3], data = load_hdf, x = x, y ='Ti', palette = palette_ti, hue = hue, legend = False)
-sns.lineplot(ax = axs[4], data = load_hdf, x = x, y ='Ti_std', palette = palette_ti, hue = hue,legend = False) 
-sns.lineplot(ax = axs[5], data = load_hdf, x = x, y ='pot', palette = palette_pot, hue = hue, legend = False)
-sns.lineplot(ax = axs[6], data = load_hdf, x = x, y ='pot_std', palette = palette_pot, hue = hue,legend = False)
-#sns.lineplot(ax = axs[7], data = load_hdf, x = x, y ='n_prob', palette = 'bone_r', hue = hue,legend = False)
-
-date_s = load_hdf['date'].iloc[0]
-date_e = load_hdf['date'].iloc[-1]
-utc_s = load_hdf['utc'].iloc[0]
-utc_e = load_hdf['utc'].iloc[-1]
-
-#axs[0].set_title(f'Equatorial Plasma Bubble: from {date_s} at {utc_s} to {date_e} at {utc_e}', fontsize = 11)
-axs[0].set_title(f'Quiet Transit: from {date_s} at {utc_s} to {date_e} at {utc_e}', fontsize = 11)
-axs[0].set_ylabel('EPB Prob')
-axs[0].set_ylim(0, 1)
-#axs[0].axhline( y=0.9, ls='-.', c='k')
-
-#left, bottom, width, height = (1, 0, 14, 7)
-#axs[4].add_patch(Rectangle((left, bottom),width, height, alpha=1, facecolor='none'))
-
-
-axs[1].set_yscale('log')
-den = r'cm$^{-3}$'
-axs[1].set_ylabel(f'Ne ({den})')
-#axs[1].axhline( y=60000, ls='-.', c='k')
-
-#axs[2].set_yscale('log')
-#axs[2].set_ylabel('Ti (K)')
-axs[2].set_ylabel('Ne \n stddev')
-#axs[2].axhline( y=950, ls='-.', c='k')
-axs[2].set_ylim(0,1)
-
-axs[3].set_ylabel('Ti (K)')
-
-
-axs[4].set_ylabel('Ti \n stddev')
-axs[4].set_ylim(0,1)
-#axs[4].set_xlabel(' ')
-#axs[4].legend(loc="center left", title="Sat")
-
-axs[5].set_ylabel('Pot (V)')
-axs[6].set_ylabel('Pot \n stddev')
-axs[6].set_ylim(0,0.5)
-
-
-axs[7].set_ylabel('IPB Prob \n proposed')
-
-#n = len(load_hdf) // 8
-#n = 50  # Keeps every 7th label
-#[l.set_visible(False) for (i,l) in enumerate(axs[7].xaxis.get_ticklabels()) if i % n != 0]
-#axs[4].tick_params(axis='x',labelrotation=90)
-#ax[0].set_xticks[]
-#axs[0].set_xticks([], minor=False)
-
-#for tic in axs[4].xaxis.get_major_ticks():
-#    tic.tick1On = tic.tick2On = True
-
-plt.tight_layout()
-plt.show()
-'''
-
-#Single Plot
-'''
-plt.figure(dpi = 90, figsize=(12,4))
-sns.scatterplot(data = load_hdf, x = 'utc', y ='Ne')
-#plt.xticks([], [])
-plt.yscale('log')
-plt.xticks(rotation=90)
-plt.tight_layout()
-plt.show()
-'''
